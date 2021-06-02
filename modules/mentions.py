cd6aa2db5a80a6eb453a4ae23c443a6aeef9f40a
kwat <jorgeglez1616@gmail.com>
2021-06-02 16:49:32 -0500

    added channel to mentions

--- modules/mentions.py
+++ modules/mentions.py
@@ -19,12 +19,15 @@ class MENTIONS(commands.Cog):
                 for role in mentions:
                     values += role.name + " " 
 
+                values += "\n"
+                values += f"\n**In channel:**  {msg.channel.name}"
+
                 uname = f"{msg.author.nick} ({msg.author.name}#{msg.author.discriminator})"
 
                 embed = Embed(title="Mentioned roles", description=values, timestamp=datetime.datetime.utcnow(), colour=0x22b7ba)
 
                 embed.set_author(name=uname, icon_url=msg.author.avatar_url)
-                
+
                 uid = f"user id: {msg.author.id} "
                 
                 embed.set_footer(text=uid)
